cbcfc177d5cfb0a27b57b98cac5f1ce494f3b016
Noah Lee <noahlee@noahs-air.attlocal.net>
2017-08-06 02:49:46 -0500

    added dadjokes

--- src/morningriver.js
+++ src/morningriver.js
@@ -1,8 +1,10 @@
 const _mama = require('../lib/yomama');
+const _dad = require('../lib/dadjokes');
 
 module.exports = class MorningRiver {
     constructor(){
         this.mama = new _mama();
+        this.dad = new _dad();
     }
 
     setLower(text) {
@@ -10,13 +12,21 @@ module.exports = class MorningRiver {
     }
 
     yoMama() {
-        var index = Math.ceil(Math.random() * (this.mama.jokes.length - 1));
+        var index = Math.ceil(Math.random() * (this.dad.jokes.length - 1));
         return this.mama.jokes[index];
+    }
+
+    puns() {
+        var index = Math.ceil(Math.random() * (this.dad.jokes.length - 1));
+        return this.dad.jokes[index];
     }
 
     mrHandler(text) {
         if(this.lower === 'yo mama' || this.lower === 'yo momma'){
             return this.yoMama();
+        }
+        else if(this.lower === 'pun' || this.lower === 'puns' || this.lower === 'dad joke'){
+            return this.puns();
         }
     }
 }
