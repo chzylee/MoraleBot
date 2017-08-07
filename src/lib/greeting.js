bef6d485b0f730170bac4deef22a4408be78de71
Noah Lee <noahlee@noahs-air.attlocal.net>
2017-08-06 20:04:55 -0500

    simplified greeting

---
+++ src/lib/greeting.js
@@ -0,0 +1,16 @@
+module.exports = class Greetings {
+    constructor() {
+        this.messages = [
+            'Salutations my little lamb.',
+            'Tis a pleasure seeing you, my delightful potato.',
+            'Whats new in the zoo cutie-poo?',
+            'My dearest baby possum, to what do I owe this honour?',
+            'How dost thou?',
+            'How fares thee mutton-muncher?',
+            'You are well met.',
+            'Hail to your grace.',
+            'Good morrow sweet kitten.',
+            'Whats poppin my dood??'
+        ]
+    }
+}
--- src/morningriver.js
+++ src/morningriver.js
@@ -1,66 +1,45 @@
 const line = require('@line/bot-sdk');
 const _mama = require('./lib/yomama');
 const _dad = require('./lib/dadjoke');
+const _greetings = require('./lib/greeting');
 
 module.exports = class MorningRiver {
     constructor(){
         this.mama = new _mama();
         this.dad = new _dad();
-        this.client = new line.Client({
-            channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
-        });
-
-        this.greetings = [
-            'salutations my little lamb.',
-            'tis a pleasure seeing you, my delightful potato.',
-            'whats new in the zoo cutie-poo?',
-            'my dearest baby possum, to what do I owe this honour?',
-            'how dost thou?',
-            'how fares thee mutton-muncher?',
-            'you are well met.',
-            'hail to your grace.',
-            'good morrow sweet kitten.',
-            'whats poppin ma dood??'
-        ]
+        this.greetings = new _greetings();
+        // this.client = new line.Client({
+        //     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+        // });
     }
 
     setLower(text) {
         this.lower = text.toLowerCase();
     }
 
-    getDisplayName(userId, client) {
-        return new Promise((resolve, reject) => {
-            var name = client.getProfile(userId)
-                .then((profile) => {
-                    return profile.displayName.toString();
-                });
+    // getDisplayName(userId, client) {
+    //     return new Promise((resolve, reject) => {
+    //         var name = client.getProfile(userId)
+    //             .then((profile) => {
+    //                 return profile.displayName.toString();
+    //             });
 
-            Promise.all([name])
-                .then((name) => {
-                    console.log('got username: ' + name[0]);
-                    resolve(name[0]);
-                })
-                .catch((err) => {
-                    console.log('error getting name: ' + err);
-                    reject(err);
-                });   
-        });
-    }
+    //         Promise.all([name])
+    //             .then((name) => {
+    //                 console.log('got username: ' + name[0]);
+    //                 resolve(name[0]);
+    //             })
+    //             .catch((err) => {
+    //                 console.log('error getting name: ' + err);
+    //                 reject(err);
+    //             });   
+    //     });
+    // }
 
-    greet(userId, greetings, client, nameFunc) {
+    greet(userId) {
         console.log('greeting user');
-        var index = Math.ceil(Math.random() * (greetings.length - 1));
-        return new Promise((resolve, reject) => {
-            nameFunc(userId, client)
-                .then((name) => {
-                    console.log('greeting user ' + name);
-                    resolve(name + ', ' + greetings[index]);
-                })
-                .catch((err) => {
-                    console.log('error getting greeting: ' + err);
-                    reject(err);
-                });
-        });
+        var index = Math.ceil(Math.random() * (this.greetings.messages.length - 1));
+        return this.greetings.messages[index];
     }
 
     yoMama() {
@@ -77,14 +56,7 @@ module.exports = class MorningRiver {
 
     mrHandler(text, userId) {
         if(this.lower.startsWith('hi') || this.lower.startsWith('hello') || this.lower.startsWith('what\'s up') || this.lower.startsWith('whats up')){
-            this.greet(userId, this.greetings, this.client, this.getDisplayName)
-                .then((result) => {
-                    console.log('got greeting: ' + result);
-                    return result;
-                })
-                .catch((err) => {
-                    console.log('error occurred: ' + err);  
-                });
+            return this.greet();
         }
         else if(this.lower.includes('yo mama') || this.lower.includes('yo momma') || this.lower.includes('yo mamma')){
             return this.yoMama();
