dc85fefdac04821e03c1659b16fce16ecf379d73
Noah Lee <noahlee@noahs-air.attlocal.net>
2017-08-11 00:41:42 -0500

    added sticker file

---
+++ src/lib/stickers.js
@@ -0,0 +1,26 @@
+module.exports = class Stickers {
+    constructor() {
+        this.fight = [
+            {
+                "type": "sticker",
+                "packageId": "2",
+                "stickerId": "517"
+            },
+            {
+                "type": "sticker",
+                "packageId": "2",
+                "stickerId": "518"
+            },
+            {
+                "type": "sticker",
+                "packageId": "2",
+                "stickerId": "519"
+            },
+            {
+                "type": "sticker",
+                "packageId": "2",
+                "stickerId": "520"
+            }
+        ]
+    }
+}
