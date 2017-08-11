e670c9d0296bcedc05e34c638871ee5bf5221c47
Noah Lee <noahlee@noahs-air.attlocal.net>
2017-08-10 23:48:56 -0500

    log message

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function handleEvent(event) {
   // create a echoing text message
   var echo = { type: 'text', text: event.message.text };
   echo.text = mr.mrHandler(echo.text, event.source.userId);
-  console.log(echo.text);
+  console.log(event.message);
   if(echo.text === 'Peace out ma doods'){
       client.replyMessage(event.replyToken, echo)
       return client.leaveGroup(event.source.groupId);
