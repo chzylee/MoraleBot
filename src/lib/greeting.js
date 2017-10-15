88934d066883eca21a58e4c4bc181a9f73fc1692
Noah Lee <noahlee@wirelessprv-10-193-206-7.near.illinois.edu>
2017-10-14 23:41:14 -0500

    simplipfied to arrays

--- src/lib/greeting.js
+++ src/lib/greeting.js
@@ -1,16 +1,12 @@
-module.exports = class Greetings {
-    constructor() {
-        this.messages = [
-            'Salutations my little lamb.',
-            'Tis a pleasure seeing you, my delightful potato.',
-            'Whats new in the zoo cutie-poo?',
-            'My dearest baby possum, to what do I owe this honour?',
-            'How dost thou?',
-            'How fares thee mutton-muncher?',
-            'You are well met.',
-            'Hail to your grace.',
-            'Good morrow sweet kitten.',
-            'Whats poppin ma dood??'
-        ]
-    }
-}
+module.exports = [
+    'Salutations my little lamb.',
+    'Tis a pleasure seeing you, my delightful potato.',
+    'Whats new in the zoo cutie-poo?',
+    'My dearest baby possum, to what do I owe this honour?',
+    'How dost thou?',
+    'How fares thee mutton-muncher?',
+    'You are well met.',
+    'Hail to your grace.',
+    'Good morrow sweet kitten.',
+    'Whats poppin ma dood??'
+];
